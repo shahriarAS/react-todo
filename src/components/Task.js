560702a717b1252ff251bd7a8e70f1f0aef45d4a
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2020-10-31 04:55:49 +0600

    Fixed Update Form text color

--- src/components/Task.js
+++ src/components/Task.js
@@ -51,7 +51,7 @@ function Task(props) {
                 {edit ? (
                     <form className="w-full max-w-sm" onSubmit={updateTask}>
                         <div className="flex items-center border-b border-teal-500 py-2">
-                            <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Jane Doe" aria-label="Full name" value={edit} onChange={event => setEdit(event.target.value)} />
+                            <input className="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Jane Doe" aria-label="Full name" value={edit} onChange={event => setEdit(event.target.value)} />
                             <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button" onClick={updateTask}>Update</button>
                         </div>
                     </form>
